ac44ff8f8c6f158e037df5fbce302d6de3f78544
tappyy <apottage@gmail.com>
2019-01-07 22:49:24 +0000

    docker config - using node-alpine for lightweight footprint

--- server/app.js
+++ server/app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('hello world'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Listening on port ${port}!`))
