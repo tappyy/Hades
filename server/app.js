4965f05914b0fb7feb6b2af640571a1c0d5553b6
tappyy <apottage@gmail.com>
2019-01-08 17:30:12 +0000

    fixed broken server :)

--- server/app.js
+++ server/app.js
@@ -3,26 +3,26 @@ const app = express()
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const port = 5000
+const port = 9000
 
 // Connection to DB
-// mongoose.connect('mongodb://localhost:27020')
-//   .then(() => {
-//     console.log('Connected to Mongo');
-//   })
-//   .catch(err => {
-//     console.error('Backend error:', err.stack);
-//     process.exit(1);
-//   });
+mongoose.connect('mongodb://localhost:27020')
+  .then(() => {
+    console.log('Connected to Mongo');
+  })
+  .catch(err => {
+    console.error('Backend error:', err.stack);
+    process.exit(1);
+  });
 
 
-// app.use(cors());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// const test = require('./src/routes/testRoute')
+const test = require('./src/routes/testRoute')
 
-app.get('/', (req, res) => res.send('hello world'))
-// app.use('/api/test', test);
+// app.get('/', (req, res) => res.send('hello world'))
+app.use('/api/test', test);
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
