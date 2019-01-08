96442a58c9a8bbbe48be13e9fa9f26efbdbae8d6
tappyy <apottage@gmail.com>
2019-01-08 22:49:17 +0000

    parsing html with beautifulsoup

--- server/src/spider/spider.py
+++ server/src/spider/spider.py
@@ -8,4 +8,11 @@ from bs4 import BeautifulSoup
 #   def __init__(self, starting_url):
 #     self.starting_url = starting_url
 
-print('i am coming from a python script')
+print('i am coming from a python script\n')
+html = requests.get('https://www.amazon.co.uk').text
+html = html.replace('\n', '')
+soup = BeautifulSoup(html, 'html.parser')
+texts = soup.get_text()
+print(texts)
+
+exit(0)
