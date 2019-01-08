9a8b1725d30804d1aa143a1530ef887f07b374bc
tappyy <apottage@gmail.com>
2019-01-08 16:05:33 +0000

    configured docker node modules and mongoose connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
 
 class App extends Component {
   render() {
+  const Header = styled.h5`
+    color: red
+  `
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code>
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Header>HELLO WORLD</Header>
     );
   }
 }
--- server/app.js
+++ server/app.js
@@ -1,7 +1,24 @@
-const express = require('express')
+const express = require('express');
 const app = express()
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const port = 5000
 
-app.get('/', (req, res) => res.send('hello world'))
+
+// Connection to DB
+mongoose.connect('mongodb://mongodb')
+  .then(() => {
+    console.log('Connected to Mongo backend');
+  })
+  .catch(err => {
+    console.error('Backend error:', err.stack);
+    process.exit(1);
+  });
+// .get('/', (req, res) => res.send('hello world'))
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
