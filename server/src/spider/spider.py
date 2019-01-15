ef00deec1f0272e5d489dcd34d8e009e794cd851
tappyy <apottage@gmail.com>
2019-01-15 23:35:45 +0000

    refactored core code - added breadth first crawling (unfinished) :hankey:

--- server/src/spider/spider.py
+++ server/src/spider/spider.py
@@ -4,7 +4,8 @@ import requests
 import sys
 from bs4 import BeautifulSoup
 from queue import Queue, Empty
-from urllib.parse import urljoin
+from collections import deque
+from urllib.parse import urljoin, urlparse
 
 
 # TODO: xpath is considerably quicker - perform benchmarks!
@@ -13,13 +14,16 @@ from urllib.parse import urljoin
 class SpiderMan:
     def __init__(self):
         self.startingUrl = 'https://hashtagmarketing.co.uk'
-        self.crawlQueue = Queue()
+        self.currentRootUrl = ''
+        self.set_root_domain(self.startingUrl)
+        # self.crawlQueue = Queue()
+        self.crawlQueue = deque([self.startingUrl])
         # self.toScrape = set() # used to convert crawlQueue to JSON (queues can't be iterated /sadface )
         self.haveScraped = set()
         # add starting url to queue
-        self.crawlQueue.put(self.startingUrl)
+        # self.crawlQueue.put(self.startingUrl)
         # set depth levels
-        self.depthLimit = 2
+        self.maxDepth = 2
         self.currentDepth = 0
 
     def make_request(self, url):
@@ -27,45 +31,56 @@ class SpiderMan:
         rawHtml = requests.get(url).text
         return rawHtml
 
-    def parse_links(self, base_url):
-        print('parsing url for links...')
-        rawHtml = self.make_request(base_url)
-        links = []  # for test output
-        soup = BeautifulSoup(rawHtml, 'html.parser')
-
-        for link in soup.find_all('a', href=True):
+    def parse_links(self, html):
+        for link in html.find_all('a', href=True):
             href = link.get('href')
             if href.startswith('/'):  # this is a sub-page of current domain
-                # convert from relative to absolute
-                absoluteUrl = urljoin(base_url, href)
-                if (href not in self.haveScraped):
-                    links.append(absoluteUrl)
-                    # self.crawlQueue.put(absoluteUrl)
-            else:
-                links.append(href)
-        self.debug_array(links)
+                absoluteUrl = urljoin(self.currentRootUrl, href)
+                self.add_to_queue(absoluteUrl)
+            elif href.startswith('http://') or href.startswith('https://'):
+                self.add_to_queue(href)
+
+    def add_to_queue(self, url):
+        if url not in self.haveScraped:
+            print(url)
+            self.crawlQueue.append(url)
+
+    def set_root_domain(self, url):
+        parsed = urlparse(url)
+        current = '{url.scheme}://{url.netloc}'.format(url=parsed)
+        print('current domain: {}'.format(current))
+        self.currentRootUrl = current
+
+    def parse_content(self, soup):
+        print(soup.get_text())
 
     def debug_array(self, stuff):
         for item in stuff:
             print(item)
 
     def run_crawler(self):
-        # while self.currentDepth < self.depthLimit:
-        while True:
+        while self.crawlQueue and self.currentDepth < self.maxDepth:
             try:
-                targetUrl = self.crawlQueue.get()
+                targetUrl = self.crawlQueue.popleft()
                 if targetUrl not in self.haveScraped:
-                    print('scraping url: {}'.format(targetUrl))
-                    self.haveScraped.add(targetUrl)
-                    # 1. get all links on current page
-                    self.parse_links(targetUrl)
-                    # 2. get current page content
+                    # set current root domain
+                    self.set_root_domain(targetUrl)
 
+                    # make request
+                    html = self.make_request(targetUrl)
+
+                    # make that beautiful soup
+                    soup = BeautifulSoup(html, 'html.parser')
+
+                    # parse links
+                    self.parse_links(soup)
+
+                    # parse content
+
+                    # set current as scraped
+                    self.haveScraped.add(targetUrl)
                     # 3. increment current depth
-                    # self.currentDepth = self.currentDepth + 1
-                    exit(0)
-            except Empty:
-                return
+                    self.currentDepth += 1
             except Exception as e:
                 print(e)
                 continue
