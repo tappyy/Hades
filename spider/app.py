3fe5c29991f57ff3b0b0459f5578fee4cf3d1e5b
tappyy <apottage@gmail.com>
2019-02-10 01:28:06 +0000

    added flask server to control spider :rocket:

--- spider/app.py
+++ spider/app.py
@@ -1,22 +1,46 @@
 from spider import SpiderMan
 import sys
 import signal
+from flask import Flask, request, jsonify
 
-# init spider
+
+# init
+app = Flask(__name__)
 spider = SpiderMan()
 
 
-def main():
-    spider.start_crawler()
+@app.route('/test')
+def test():
+    return jsonify({'message': 'API is working!'})
+
+
+@app.route('/start')
+def start_spider():
+    spider.start()
+    # FIXME: will not return because spider.start() is blocking thread
+    return jsonify({'message': 'Spider started.'})
+
+
+@app.route('/stop')
+def stop_spider():
+    spider.stop()
+    return jsonify({'message': 'Spider stopped.'})
+
+
+@app.route('/stats')
+def get_stats():
+    # get running stats from spider
+    return jsonify({'stats': 'These are placeholder stats'})
 
 
 def signal_handler(sig, frame):
     print('Stopping spider...')
-    spider.stop_crawler()
+    spider.stop()
     sys.exit(0)
 
 
 signal.signal(signal.SIGINT, signal_handler)
 
 if __name__ == '__main__':
-    main()
+    # main()
+    app.run(debug=True, port=9001)
--- spider/spider.py
+++ spider/spider.py
@@ -20,80 +20,80 @@ from utils.constants import QUEUE_FILE, STARTING_QUEUE
 # - number of crawled urls
 # - total up time
 # - pages crawled per second (for benchmarking multi-threading etc (create graphs))
+# TODO: move spider to seperate thread from server
 
 
 class SpiderMan:
     def __init__(self):
 
-        # remove_file(QUEUE_FILE)
-        # self.startingUrl = 'http://33cb5x4tdiab2jhe.onion/'
-        self.currentRootUrl = ''
-        # self.set_root_domain(self.startingUrl)
-        self.crawlQueue = deque([])
-        self.urlsTodo = set()
-        self.haveScraped = set()
-        self.isCrawling = False
+        self.__currentRootUrl = ''
+        self.__crawlQueue = deque([])
+        self.__urlsTodo = set()
+        self.__haveScraped = set()
+        self.__isCrawling = False
 
         # init queue
-        self.init_queue()
+        # self.__init_queue()
 
         # set depth levels
-        self.maxDepth = 3
-        self.currentDepth = 0
+        self.__maxDepth = 3
+        self.__currentDepth = 0
 
-        # init session
-        self.session = requests.session()
-        self.session.proxies = {}
-        self.session.proxies['http'] = 'socks5h://localhost:9150'
-        self.session.proxies['https'] = 'socks5h://localhost:9150'
+        # init __session
+        self.__session = requests.session()
+        self.__session.proxies = {}
+        self.__session.proxies['http'] = 'socks5h://localhost:9150'
+        self.__session.proxies['https'] = 'socks5h://localhost:9150'
 
-    def init_queue(self):
+        self.__run_crawler()
+
+    def __init_queue(self):
         if file_exists(QUEUE_FILE):
             print('found queue file - using that')
             queue = build_queue_from_txt(QUEUE_FILE)
-            self.crawlQueue.extend(queue)
+            self.__crawlQueue.extend(queue)
         else:
             print('using starting urls')
-            self.crawlQueue.extend(STARTING_QUEUE)
+            self.__crawlQueue.extend(STARTING_QUEUE)
 
-    def make_request(self, url):
+    def __make_request(self, url):
         print('requesting: {}'.format(url))
         # remove any cookies
-        self.session.cookies.clear()
+        self.__session.cookies.clear()
 
-        # set session headers
+        # set __session headers
         headers = {}
         headers['User-agent'] = 'Chrome'
 
         try:
-            r = self.session.get(url, headers=headers, timeout=(3, 30))
+            r = self.__session.get(url, headers=headers, timeout=(3, 30))
             if r:
                 return r.text
         except requests.RequestException as error:
             print(error)
             return
 
-    def parse_links(self, html):
+    def __parse_links(self, html):
         for link in html.find_all('a', href=True):
             href = link.get('href')
             if href.startswith('/'):  # this is a sub-page of current domain
-                absoluteUrl = urljoin(self.currentRootUrl, href)
-                self.add_to_queue(absoluteUrl)
+                absoluteUrl = urljoin(self.__currentRootUrl, href)
+                self.__add_to_queue(absoluteUrl)
             elif href.startswith('http://') or href.startswith('https://'):
-                self.add_to_queue(href)
+                self.__add_to_queue(href)
 
-    def add_to_queue(self, url):
-        if url not in self.haveScraped:
-            self.urlsTodo.add(url)
+    def __add_to_queue(self, url):
+        if url not in self.__haveScraped:
+            self.__urlsTodo.add(url)
 
-    def set_root_domain(self, url):
+    def __set_root_domain(self, url):
         parsed = urlparse(url)
         current = '{url.scheme}://{url.netloc}'.format(url=parsed)
-        self.currentRootUrl = current
+        self.__currentRootUrl = current
 
-    def parse_content(self, soup, url):
+    def __parse_content(self, soup, url):
         data = {
-            "website_root": self.currentRootUrl,
+            "website_root": self.__currentRootUrl,
             "page_url": url,
             "page_title": soup.title.string,
             "body_content": soup.get_text()
@@ -101,64 +101,63 @@ class SpiderMan:
 
         requests.post('http://localhost:9000/api/pages', data=data)
 
-    def debug_array(self, stuff):
+    def __debug_array(self, stuff):
         for item in stuff:
             print(item)
 
-    def end_of_level(self):
-        if(self.currentDepth < self.maxDepth):
+    def __end_of_level(self):
+        if(self.__currentDepth < self.__maxDepth):
 
             # queue next list of links
-            self.crawlQueue.clear()
-            self.crawlQueue.extend(self.urlsTodo)
-            self.urlsTodo.clear()
+            self.__crawlQueue.clear()
+            self.__crawlQueue.extend(self.__urlsTodo)
+            self.__urlsTodo.clear()
 
             # update current depth
-            self.currentDepth += 1
+            self.__currentDepth += 1
         else:
             exit(0)
 
-    def stop_crawler(self):
-        self.isCrawling = False
-        save_deque_to_txt(self.crawlQueue)
-
-    def start_crawler(self):
-        # TODO: configure crawler queue
-        #  check if queue file exists, if does, configure queue from file, else use starting queue
-
-        # start crawler
-        print('Spider status: crawling')
-        self.isCrawling = True
-        self.run_crawler()
-
-    def run_crawler(self):
-        while self.isCrawling:
-            if self.crawlQueue:
+    def __run_crawler(self):
+        while self.__isCrawling:
+            if self.__crawlQueue:
                 try:
-                    targetUrl = self.crawlQueue.popleft()
-                    if targetUrl not in self.haveScraped:
+                    targetUrl = self.__crawlQueue.popleft()
+                    if targetUrl not in self.__haveScraped:
                         # set current root domain
-                        self.set_root_domain(targetUrl)
+                        self.__set_root_domain(targetUrl)
 
                         # make request
-                        html = self.make_request(targetUrl)
+                        html = self.__make_request(targetUrl)
                         if html is not None:
                             # make that beautiful soup
                             soup = BeautifulSoup(html, 'html.parser')
 
                             # parse links
-                            self.parse_links(soup)
+                            self.__parse_links(soup)
 
                             # parse content
-                            self.parse_content(soup, targetUrl)
+                            self.__parse_content(soup, targetUrl)
 
                             # set current as scraped
-                            self.haveScraped.add(targetUrl)
+                            self.__haveScraped.add(targetUrl)
                         else:
                             continue
                 except Exception as error:
                     print(error)
                     continue
             else:
-                print('end of level: {}'.format(self.currentDepth))
-                self.end_of_level()
+                print('end of level: {}'.format(self.__currentDepth))
+                self.__end_of_level()
+
+    def stop(self):
+        if self.__isCrawling:
+            self.__isCrawling = False
+            save_deque_to_txt(self.__crawlQueue)
+
+    def start(self):
+        if not self.__isCrawling:
+            self.__init_queue()
+            print('Spider status: crawling')
+            self.__isCrawling = True
+            self.__run_crawler()
