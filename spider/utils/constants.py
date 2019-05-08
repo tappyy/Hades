324a8d0ab89f3e15c39505aa3a01dcc50812389c
tappyy <apottage@gmail.com>
2019-05-08 22:12:44 +0100

    updated constants for spider

--- spider/utils/constants.py
+++ spider/utils/constants.py
@@ -1,7 +1,7 @@
 QUEUE_FILE = 'queue.txt'
 CONFIG_FILE = 'config.ini'
 SERVER_PAGES_API = 'http://localhost:9000/api/pages'
-MAX_DEPTH = 3
+MAX_DEPTH = 4
 STARTING_QUEUE = [
     'http://33cb5x4tdiab2jhe.onion/'
 ]
